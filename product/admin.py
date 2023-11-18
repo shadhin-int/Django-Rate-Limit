176287497621257ef09f408b335bf04f8e30bdd8
shadhin-int <shadhin.int@gmail.com>
2023-11-18 14:50:17 +0600

    configure admin panel

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
+from product.models import Product
+
 # Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'name',
+        'price',
+        'color',
+        'weight',
+        'created_at',
+        'updated_at',
+    )
+
+    list_filter = (
+        'color',
+        'created_at'
+    )
+
+    date_hierarchy = 'created_at'

0cf70e0b5e505b58e9936a26042a17124984c5bb
shadhin-int <shadhin.int@gmail.com>
2023-11-18 13:35:27 +0600

    implement product create, list and delete view

--- product/urls.py
+++ product/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 
+from product.views import CreateProductView, GetProductsView, DeleteProductView
+
 urlpatterns = [
-    
+    path("create", CreateProductView.as_view()),
+    path("get", GetProductsView.as_view()),
+    path("delete", DeleteProductView.as_view())
+
 ]
--- product/views.py
+++ product/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework.generics import CreateAPIView, ListAPIView, DestroyAPIView
 
-# Create your views here.
+from product.models import Product
+from product.serializers import ProductSerializer
+
+
+class CreateProductView(CreateAPIView):
+    serializer_class = ProductSerializer
+    queryset = Product.objects.all()
+
+
+class GetProductsView(ListAPIView):
+    serializer_class = ProductSerializer
+    queryset = Product.objects.all()
+
+
+class DeleteProductView(DestroyAPIView):
+    serializer_class = ProductSerializer
+    queryset = Product.objects.all()
